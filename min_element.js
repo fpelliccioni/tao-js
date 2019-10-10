52291bd73b22c11e910a9dca4a9b9fe7934777d6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-10-09 22:57:40 -0300

    Create min_element.js

---
+++ min_element.js
@@ -0,0 +1,16 @@
+
+function min_element(f, l, r) {
+    if (equal(f, l)) return l;
+
+    var m = f;
+    f = successor(f);
+
+    while ( ! equal(f, l)) {
+        if (r(source(f), source(m))) {
+            m = f;
+        }
+        f = successor(f);
+    }
+    return m;
+}
+​
