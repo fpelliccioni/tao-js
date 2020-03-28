513ca5bb860e983420424fc52781462666c20b85
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-03-27 23:58:23 -0300

    fixed partition point n experimental usage

--- build/algorithms_extra.js
+++ build/algorithms_extra.js
@@ -686,19 +686,20 @@ function __partition_point_n_attributes() {
 
 function __partition_point_n_forward_usage() {
     var eq3 = predicate(function eq3(x) {return x == 3;});
-    var lt3 = predicate(function lt3(x) {return x < 3;});
+    var gt3 = predicate(function gt3(x) {return x > 3;});
     var gte3 = predicate(function gte3(x) {return x >= 3;});
     
-    var d = sequence([1, 5, 1, 1, 3, 3, 3, 7, 3, 2, 6, 4], "d", eq3);
+    var d = sequence([1, 1, 1, 1, 3, 3, 3, 3, 3, 7, 8, 9], "d", eq3);
     
-    var lb = partition_point_n_forward(begin(d), size(d), lt3);
-    var ub = partition_point_n_forward(lb[0], lb[1], gte3);
+    var lb = partition_point_n_forward(begin(d), size(d), gte3);
+    var ub = partition_point_n_forward(lb[0], lb[1], gt3);
 
     var f = lb[0];
     var l = ub[0];
 
     while ( ! equal(f, l)) {
         print(source(f) + ", ");
+        f = successor(f);
     }
 }
 
