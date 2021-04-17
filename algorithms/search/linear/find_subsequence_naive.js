726785578d9bbb4c85f67abab695b780a7351771
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-17 09:14:30 -0300

    find_subsequence for bounded ranges

---
+++ algorithms/search/linear/find_subsequence_naive.js
@@ -0,0 +1,38 @@
+function find_subsequence_naive(f, l, sf, sl, r) {
+    if (equal(sf, sl)) return f;
+
+    while (true) {
+        var tf = f;
+        var ts = sf;
+        while (true) {
+            if (equal(tf, l)) return l;
+            if (equal(ts, sl)) return f;
+            if ( ! r(source(tf), source(ts))) break;
+            ts = successor(ts);
+            tf = successor(tf);
+        }
+        f = successor(f);
+    }
+    return f;
+}
+
+function usage() {
+
+
+    var seq = sequence(array_from("Hello, World!"), "seq", undefined, "list");
+    var sub = sequence(array_from("????"), "sub", undefined, "list");
+
+    var it = find_subsequence_naive(begin(seq), end(seq), begin(sub), end(sub), eq);
+
+    if (equal(it, end(seq))) {
+        print("Subsequence not found");
+    } else {
+        print("Subsequence found at: ");
+        print(distance(begin(seq), it));
+    }
+}
+
+function attributes() {
+
+}
+
