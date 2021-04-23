efa66390b814327daac03cfac312a9159725a607
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-23 14:36:06 -0300

    fix

--- algorithms/copying/predicate/copy_if.js
+++ algorithms/copying/predicate/copy_if.js
@@ -1,13 +1,13 @@
-function predicate_source(p) {
-    return function(x) { return p(source(x)); }
-}
-
 function copy_if(f, l, o, p) {
     var ps = predicate_source(p);
     return copy_select(f, l, o, ps);
 }
 
 function usage() {
+    function predicate_source(p) {
+        return function(x) { return p(source(x)); }
+    }
+
     var even = predicate(function even(x) { return (x & 1) == 0; });
     var s = sequence(array_random(), "s", even);
     var z = sequence(array_all_equal(size(s), "-"), "z");
--- build/algorithms_extra.js
+++ build/algorithms_extra.js
@@ -206,6 +206,10 @@ function __reverse_copy_n_attributes() {
 }
 
 function __copy_if_usage() {
+    function predicate_source(p) {
+        return function(x) { return p(source(x)); }
+    }
+
     var even = predicate(function even(x) { return (x & 1) == 0; });
     var s = sequence(array_random(), "s", even);
     var z = sequence(array_all_equal(size(s), "-"), "z");
