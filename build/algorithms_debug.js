4da10fb57410d7dbe3ee9f49fe36653206880226
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-24 20:01:20 -0300

    fixes

--- build/algorithms_debug.js
+++ build/algorithms_debug.js
@@ -1571,6 +1571,6 @@ function half_nonnegative(n) {return n >> 1;}
 function twice(n) {return n + n;}
 function remainder(a, b) {return a % b;}var eq = relation(function eq(x, y) {return x == y;});
 var lt = relation(function lt(x, y) {return x < y;});
-var gt = relation(function gt(x, y) {return x < y;});
+var gt = relation(function gt(x, y) {return x > y;});
 var lte = relation(function lte(x, y) {return x <= y;});
 var gte = relation(function gte(x, y) {return x >= y;});
