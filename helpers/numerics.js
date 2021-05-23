9e738b7bec977fa8c49c848f1cb3fe04a2d1ff0b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-05-23 13:11:35 -0300

    some numerics helpers

--- helpers/numerics.js
+++ helpers/numerics.js
@@ -1,3 +1,6 @@
 function half_nonnegative(n) {return n >> 1;}
+function half(n) {return n >> 1;}
 function twice(n) {return n + n;}
 function remainder(a, b) {return a % b;}
+function even(x) { return (x & 1) == 0; }
+function odd(x) { return ! even(x); }
