4a038f4b073f45f944393fcc7a7be3afb604b57e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-06-08 10:06:29 -0300

    move some algos to misc

---
+++ algorithms/numerics/misc/divides.js
@@ -0,0 +1,18 @@
+function divides(i, n) {
+    return n % i == 0;
+}
+
+function usage() {
+    var i = random_int();
+    var n = random_int();
+    print(divides(i, n));
+
+    i = 5;
+    n = 15;
+    print(divides(i, n));
+}
+
+function attributes() {
+
+}
+
