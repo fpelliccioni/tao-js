4e1cd3328952dcfe6d982d9c0ce5efa7095dab44
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-10-17 19:52:10 +0200

    adding relation helpers

--- helpers/relations.js
+++ helpers/relations.js
@@ -3,3 +3,19 @@ var lt = relation(function lt(x, y) {return x < y;});
 var gt = relation(function gt(x, y) {return x > y;});
 var lte = relation(function lte(x, y) {return x <= y;});
 var gte = relation(function gte(x, y) {return x >= y;});
+
+function complement(r) {
+    return function(a, b) {
+        return !r(a, b);
+    };
+}
+
+function converse(r) {
+    return function(a, b) {
+        return r(b, a);
+    };
+}
+
+function complement_of_converse(r) {
+    return complement(converse(r));
+}
